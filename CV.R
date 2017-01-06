0071d76c04b23ec4afd2d3729d2041f505ef2d72
FM2805 <florian.messner@zoho.com>
2017-01-06 20:46:49 +0100

    Update CV.R

--- CV.R
+++ CV.R
@@ -4,18 +4,18 @@
 # Replace 'df', 'Target', 'x', 'Var' and 'model_list'
 ########################
 
-Score <- function(my_model) {
+CV_Score <- function(my_model) {
   for (i in 1:length(df$'Target')-1){ 
     # Define the data frame without the respective year
-    newframe <- subset(df,'Var'!='x'-i)
+    df.newframe <- subset(df,'Var'!='x'-i)
     #Estimate model for remaining sample
-    b1<- gam(my_model,data=newframe)
+    b1<- gam(my_model,data=df.newframe)
     #Get Subset for which we want to predict and calc. y_hat
-    specific<-subset(df,'Var'=='x'-i)
+    df.specific<-subset(df,'Var'=='x'-i)
     y_hat <-predict(b1,type="response",newdata=new)
-    y_true <- specific$'Target'
+    y_real <- df.specific$'Target'
     #Get the squared error and calculate the mean
-    error_sq <- (y_true-y_hat)^2
+    error_sq <- (y_real-y_hat)^2
     m_value[i] = mean(error_sq)
   }
   #Calculate Mean of the Mean Squared Error as the final CV score 
@@ -28,6 +28,6 @@ CV<-rep(NA,'models')
 model_list <- c(my_model1, my_model2,my_model3, my_model4,..)
 k = 1
 for (my_model in model_list) {
-  CV[k] <-Score(my_model)
+  CV[k] <-CV_Score(my_model)
   k = k+1
 }
