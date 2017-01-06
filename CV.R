c662d27ea637269f3c4621f4eb08077bf7c38874
FM2805 <florian.messner@zoho.com>
2017-01-06 20:35:17 +0100

    Add files via upload

---
+++ CV.R
@@ -0,0 +1,33 @@
+########################
+# Function for Leave-One-Out Cross Validation.
+# Example with a Generalized Additive Model.
+# Replace 'df', 'Target', 'x', 'Var' and 'model_list'
+########################
+
+Score <- function(my_model) {
+  for (i in 1:length(df$'Target')-1){ 
+    # Define the data frame without the respective year
+    newframe <- subset(df,'Var'!='x'-i)
+    #Estimate model for remaining sample
+    b1<- gam(my_model,data=newframe)
+    #Get Subset for which we want to predict and calc. y_hat
+    specific<-subset(df,'Var'=='x'-i)
+    y_hat <-predict(b1,type="response",newdata=new)
+    y_true <- specific$'Target'
+    #Get the squared error and calculate the mean
+    error_sq <- (y_true-y_hat)^2
+    m_value[i] = mean(error_sq)
+  }
+  #Calculate Mean of the Mean Squared Error as the final CV score 
+  CV_Score <- mean(m_value)
+  return(CV_Score)
+}  
+
+m_value<-rep(NA,length(df$'Target')-1)
+CV<-rep(NA,'models')
+model_list <- c(my_model1, my_model2,my_model3, my_model4,..)
+k = 1
+for (my_model in model_list) {
+  CV[k] <-Score(my_model)
+  k = k+1
+}
